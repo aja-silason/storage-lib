54591cef84444c0d0ce13b0674e3a8a8709d0953
anania augusto <100473023+aja-silason@users.noreply.github.com>
2024-10-23 22:51:56 +0100

    Delete src/test.js

--- src/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-(0, index_1.setStorageData)("MyKey", "MyValue");
-const getValue = (0, index_1.getStorageData)("MyKey");
-console.log("result: ", getValue);
-(0, index_1.removeStorageData)("MyKey");

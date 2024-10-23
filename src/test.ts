73c5af0546cbd93b6ab1ba698bff5327b9f88fce
anania augusto <100473023+aja-silason@users.noreply.github.com>
2024-10-23 22:52:31 +0100

    Delete src/test.ts

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import {getStorageData, setStorageData, removeStorageData} from "./index";
-
-setStorageData("MyKey", "MyValue");
-
-const getValue = getStorageData("MyKey");
-
-console.log("result: ", getValue);
-
-removeStorageData("MyKey");
